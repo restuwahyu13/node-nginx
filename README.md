706158dde7c1b625ef0ff1be9eab0b302ea09f3f
restuwahyu13 <restuwahyu13@gmail.com>
2021-02-04 17:58:05 +0700

    publish files

---
+++ README.md
@@ -0,0 +1,3 @@
+# Node Nginx
+
+simple setup nodejs + nginx on docker with docker.compose.yml
---
+++ index.js
@@ -0,0 +1,16 @@
+const http = require('http')
+const server = http.createServer((req, res) => {
+
+ res.writeHead(200, {'content-type': 'text/html'})
+
+ if(req.url === '/') {
+ 	res.write('<h1>Node and Nginx on Docker is Working</h1>')
+ 	res.end()
+ } else {
+ 	res.write('<h1>404 Nout Found</h1>')
+ 	res.end()
+ }
+
+})
+
+server.listen(process.env.PORT || 3000, () => console.log(`server running on ${server.address().port}`))
