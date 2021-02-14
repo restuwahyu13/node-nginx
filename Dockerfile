8960b72df1b2006f0fef6a9c9b1a98ef8743c468
anti-malware <restuwahyu13@gmail.com>
2021-02-14 22:33:27 +0700

    update dockerfile, docker-compose and nginx

--- Dockerfile
+++ Dockerfile
@@ -2,10 +2,10 @@ FROM node:14-alpine
 
 WORKDIR /usr/src/app
 
-COPY package*.json ./
+COPY package*.json /usr/src/app
 
 RUN npm install
 
-COPY . ./
+COPY . /usr/src/app
 
 CMD npm start
--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 const http = require('http')
 const server = http.createServer((req, res) => {
+	res.writeHead(200, { 'content-type': 'text/html' })
 
- res.writeHead(200, {'content-type': 'text/html'})
-
- if(req.url === '/') {
- 	res.write('<h1>Node and Nginx on Docker is Working</h1>')
- 	res.end()
- } else {
- 	res.write('<h1>404 Nout Found</h1>')
- 	res.end()
- }
-
+	if (req.url === '/') {
+		res.write('<h1>Node and Nginx on Docker is Working</h1>')
+		res.end()
+	} else {
+		res.write('<h1>404 Nout Found</h1>')
+		res.end()
+	}
 })
 
 server.listen(process.env.PORT || 3000, () => console.log(`server running on ${server.address().port}`))
